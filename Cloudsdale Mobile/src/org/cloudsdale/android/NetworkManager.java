4ef9649f4b0493ccc62f9689597a0ca250a8120f
Jamison Greeley <atomicrat2552@gmail.com>
2012-09-25 06:49:39 -0600

    Started work on replacing stored user with a system-wide authenticator

---
+++ Cloudsdale Mobile/src/org/cloudsdale/android/NetworkManager.java
@@ -0,0 +1,69 @@
+package org.cloudsdale.android;
+
+import java.util.ArrayList;
+
+import org.apache.http.message.BasicNameValuePair;
+import org.cloudsdale.android.exceptions.CloudsdaleQueryException;
+import org.cloudsdale.android.models.LoggedUser;
+import org.cloudsdale.android.models.QueryData;
+import org.cloudsdale.android.models.authentication.Provider;
+import org.cloudsdale.android.models.queries.SessionQuery;
+
+public class NetworkManager {
+
+	private static LoggedUser sLoggedUser;
+	private static CloudsdaleQueryException sExceptionThrown;
+
+	/*
+	 * Public method to authenticate users via Cloudsdale credentials
+	 */
+	public static LoggedUser authenticate(String email, String password)
+			throws CloudsdaleQueryException {
+		// TODO authenticate using Cloudsdale credentials
+		// Reset the exception
+		sExceptionThrown = null;
+
+		// Get the api endpoint
+		final String apiBase = Cloudsdale.getContext().getString(
+				R.string.cloudsdale_api_base);
+		final String sessionEndpoint = Cloudsdale.getContext().getString(
+				R.string.cloudsdale_sessions_endpoint);
+		final String apiUrl = apiBase + sessionEndpoint;
+
+		// Build the query data
+		final QueryData data = new QueryData();
+		data.setUrl(apiUrl);
+		ArrayList<BasicNameValuePair> nameValuePairs = new ArrayList<BasicNameValuePair>(
+				2);
+		nameValuePairs.add(new BasicNameValuePair("email", email));
+		nameValuePairs.add(new BasicNameValuePair("password", password));
+		data.setHeaders(nameValuePairs);
+
+		// Execute the query
+		new Thread() {
+			public void run() {
+				SessionQuery query = new SessionQuery(apiUrl);
+				try {
+					sLoggedUser = query.execute(data, Cloudsdale.getContext());
+				} catch (CloudsdaleQueryException e) {
+					sExceptionThrown = e;
+				}
+			};
+		}.start();
+		if (sExceptionThrown != null) {
+			return sLoggedUser;
+		} else {
+			throw sExceptionThrown;
+		}
+	}
+
+	/*
+	 * Public method to authenticate users via oAuth credentials (e.g. Twitter,
+	 * Facebook)
+	 */
+	public static LoggedUser authenticate(String oAuthId, Provider oAuthProvider) {
+		// TODO authenticate using oAuth credentials
+		return null;
+	}
+
+}
---
+++ Cloudsdale Mobile/tests/src/org/cloudsdale/android/test/QueryTests.java
@@ -0,0 +1,168 @@
+package org.cloudsdale.android.test;
+
+import android.test.AndroidTestCase;
+import android.util.Log;
+
+import org.apache.http.message.BasicNameValuePair;
+import org.cloudsdale.android.models.LoggedUser;
+import org.cloudsdale.android.models.QueryData;
+import org.cloudsdale.android.models.api_models.Cloud;
+import org.cloudsdale.android.models.api_models.Drop;
+import org.cloudsdale.android.models.api_models.Message;
+import org.cloudsdale.android.models.api_models.User;
+import org.cloudsdale.android.models.queries.ChatMessageGetQuery;
+import org.cloudsdale.android.models.queries.CloudGetQuery;
+import org.cloudsdale.android.models.queries.DropGetQuery;
+import org.cloudsdale.android.models.queries.MessagePostQuery;
+import org.cloudsdale.android.models.queries.SessionQuery;
+import org.cloudsdale.android.models.queries.UserGetQuery;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import junit.framework.Assert;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+public class QueryTests extends AndroidTestCase {
+
+    public void testSessionQuery_Username() {
+        LoggedUser user = establishSession();
+
+        // Assert
+        Assert.assertNotNull(user);
+        Assert.assertEquals("Cloudsdale Dummy Account", user.getName());
+    }
+
+    public void testSessionQuery_FB() {
+
+    }
+
+    public void testSessionQuery_TW() {
+
+    }
+
+    public void testUserLookupQuery() {
+        // Arrange
+        QueryData data = setupQueryData(Constants.DUMMY_LOOKUP_ENDPOINT);
+        LoggedUser session = establishSession();
+
+        // Act
+        UserGetQuery query = new UserGetQuery(data.getUrl());
+        query.addHeader("X-AUTH-TOKEN", session.getAuthToken());
+        User user = query.execute(data, null);
+
+        // Assert
+        Assert.assertNotNull(user);
+        Assert.assertEquals("Cloudsdale Dummy Account", user.getName());
+    }
+
+    public void testCloudLookupQuery() {
+        // Arrange
+        QueryData data = setupQueryData(Constants.HAMMOCK_LOOKUP_ENDPOINT);
+        LoggedUser session = establishSession();
+
+        // Act
+        CloudGetQuery query = new CloudGetQuery(data.getUrl());
+        query.addHeader("X-AUTH-TOKEN", session.getAuthToken());
+        Cloud cloud = query.execute(data, null);
+
+        // Assert
+        Assert.assertNotNull(cloud);
+        Log.d("Cloud Lookup Test", cloud.getName());
+        Assert.assertEquals("Hammock", cloud.getName());
+    }
+
+    public void testCloudChatQuery() {
+        // Arrange
+        QueryData data = setupQueryData(Constants.META_CHAT_ENDPOINT);
+        LoggedUser session = establishSession();
+
+        // Act
+        ChatMessageGetQuery query = new ChatMessageGetQuery(data.getUrl());
+        query.addHeader("X-AUTH-TOKEN", session.getAuthToken());
+        Message[] messages = query.executeForCollection(data, null);
+
+        // Assert
+        Assert.assertNotNull(messages);
+    }
+
+    public void testMessageSendQuery() {
+        // Arrange
+        QueryData data = setupQueryData(Constants.TEST_CHAT_ENDPOINT);
+        LoggedUser session = establishSession();
+
+        // Act
+        MessagePostQuery query = new MessagePostQuery(data.getUrl());
+        JSONObject body = new JSONObject();
+        JSONObject message = new JSONObject();
+        String content = "Testing, 123 testing! This is an automated unit test running on Android! BOING BOING! THE TIME IS NOW "
+                + Calendar.getInstance().getTime().toString()
+                .toUpperCase();
+        try {
+            body.put("content", content);
+            body.put("device", "mobile");
+            body.put("client_id", session.getId());
+            message.put("message", body);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        data.setJson(message.toString());
+        query.addHeader("X-AUTH-TOKEN", session.getAuthToken());
+        Message result = query.execute(data, null);
+
+        // Assert
+        Log.d("Message Post Test", message.toString());
+        Assert.assertNotNull(result);
+        Assert.assertEquals(content, result.getContent());
+    }
+    
+    public void testDropFetch() {
+        // Arrange
+        QueryData data = setupQueryData(Constants.META_DROP_ENDPOINT);
+        LoggedUser session = establishSession();
+        
+        // Act
+        DropGetQuery query = new DropGetQuery(data.getUrl());
+        query.addHeader("X-AUTH-TOKEN", session.getAuthToken());
+        Drop[] result = query.executeForCollection(data, null);
+        
+        // Assert
+        Assert.assertNotNull(result);
+        Assert.assertEquals(10, result.length);
+        for(Drop drop : result) {
+            Assert.assertNotNull(drop);
+            Assert.assertNotNull(drop.getId());
+            Assert.assertNotNull(drop.getPreview());
+            Assert.assertNotNull(drop.getTitle());
+            Assert.assertNotNull(drop.getUrl());
+            Assert.assertNotNull(drop.getStatus());
+            Assert.assertEquals("200", drop.getStatus()[0]);
+            Assert.assertEquals("OK", drop.getStatus()[1]);
+        }
+    }
+    
+    private QueryData setupQueryData(String url) {
+        QueryData data = new QueryData();
+        data.setUrl(url);
+        return data;
+    }
+
+    private QueryData setupQueryData_Username(String url) {
+        QueryData data = setupQueryData(url);
+        data.setUrl(Constants.SESSION_ENDPOINT);
+        ArrayList<BasicNameValuePair> headers = new ArrayList<BasicNameValuePair>();
+        headers.add(new BasicNameValuePair("email", Constants.DUMMY_EMAIL));
+        headers.add(new BasicNameValuePair("password", Constants.DUMMY_PASSWORD));
+        data.setHeaders(headers);
+        return data;
+    }
+
+    private LoggedUser establishSession() {
+        QueryData data = setupQueryData_Username(Constants.SESSION_ENDPOINT);
+        SessionQuery sq = new SessionQuery(data.getUrl());
+        LoggedUser session = sq.execute(data, null);
+        return session;
+    }
+}
