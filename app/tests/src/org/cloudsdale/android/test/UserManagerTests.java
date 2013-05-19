d4148afa360ade0e5db1cb2326eef7d5252e4790
Jamison Greeley <atomicrat2552@gmail.com>
2013-05-18 18:48:35 -0600

    Finally fix that stupid compile error

--- app/tests/src/org/cloudsdale/android/test/UserManagerTests.java
+++ app/tests/src/org/cloudsdale/android/test/UserManagerTests.java
@@ -1,7 +1,6 @@
 package org.cloudsdale.android.test;
 
 import android.test.ApplicationTestCase;
-
 import org.cloudsdale.android.Cloudsdale;
 import org.cloudsdale.android.models.api.User;
 import org.cloudsdale.android.models.exceptions.QueryException;
@@ -19,14 +18,9 @@ public class UserManagerTests extends ApplicationTestCase<Cloudsdale> {
 	}
 
 	public void testUserGet() {
-		try {
-			User u = getApplication().getUserManager().getUserById(
-					Constants.DUMMY_ID);
-			assertNotNull(u);
-			assertEquals(Constants.DUMMY_EMAIL, u.getEmail());
-		} catch (QueryException e) {
-			fail();
-		}
+        User u = getApplication().getUserDataStore().get(Constants.DUMMY_ID);
+        assertNotNull(u);
+        assertEquals(Constants.DUMMY_EMAIL, u.getEmail());
 	}
 
 }
